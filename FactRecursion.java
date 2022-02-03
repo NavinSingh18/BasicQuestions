20259d2b095cee6c7d57515028a9cb735ca24e09
Navin Singh <98936244+NavinSingh18@users.noreply.github.com>
2022-02-03 12:13:58 +0530

    Update FactRecursion.java

--- FactRecursion.java
+++ FactRecursion.java
@@ -10,7 +10,7 @@ public class FactRecursion {
 
     static int factorial(int n){
         if(n>1) {
-            return n * factorial(n - 1);
+            return n * factorial(n - 1);    // return (n==0 || n==1)?1:n*factorial(n-1);
         }
         return 1;
     }
