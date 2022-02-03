9b291f5f78b8bd094c3fcc54f28b10c56062292f
Navin Singh <98936244+NavinSingh18@users.noreply.github.com>
2022-02-03 11:41:27 +0530

    Update FactRecursion.java

--- FactRecursion.java
+++ FactRecursion.java
@@ -1,1 +1,17 @@
+import java.util.*;
+public class FactRecursion {
 
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        long fact = factorial(number);
+        System.out.println("Factorial of "+ number + " is " + fact);
+    }
+
+    static int factorial(int n){
+        if(n>1) {
+            return n * factorial(n - 1);
+        }
+        return 1;
+    }
+}
