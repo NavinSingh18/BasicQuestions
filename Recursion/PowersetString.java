54df08771b3c0963b1a33bceb17242a4531f77dd
Navin Singh <98936244+NavinSingh18@users.noreply.github.com>
2022-05-25 10:24:30 +0530

    Create PowersetString.java

---
+++ Recursion/PowersetString.java
@@ -0,0 +1,29 @@
+public class MyClass {
+    public static void main(String args[]) {
+        String s = "racecar";
+        int i =0;
+        String cur=" ";
+        int r = s.length()-1;
+       powerSet(s,i,cur);
+
+
+        
+
+    }
+
+    static void powerSet(String s, int i, String cur){
+        int count=0;
+        if(i==s.length()){
+            
+            System.out.print(cur);
+            
+            return ;
+        }
+        
+        else{
+             powerSet(s, i+1, cur+s.charAt(i));
+             powerSet(s, i+1, cur);
+        }
+    }
+
+}
