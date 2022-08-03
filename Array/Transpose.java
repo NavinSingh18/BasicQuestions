d08ec05c466a5c4600019dc3f0e6d1b1a87f1b86
Navin Singh <98936244+NavinSingh18@users.noreply.github.com>
2022-08-03 20:08:19 +0530

    Create Transpose.java

---
+++ Array/Transpose.java
@@ -0,0 +1,30 @@
+public class MyClass {
+    public static void main(String args[]) {
+     //int original[][]={{1,3},{2,4},{2,4},{2,4}};
+     int original [][] = {       
+                        {4, 2, 5},    
+                        {1, 5, 4},    
+                        {1, 2, 9}    
+                    };
+      
+     transpose(original);
+    }
+  
+  static void transpose(int[][] arr){
+        int rows = arr.length;
+        int cols = arr.length;
+        int[][] arr1 = new int[rows][cols];
+        
+        for(int i =0; i<cols; i++){
+            for(int j=0; j<rows; j++){
+                arr1[i][j] = arr[j][i];
+        }
+    }
+    for(int i =0; i<cols; i++){
+            for(int j=0; j<rows; j++){
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println();
+            
+       }
+  }
